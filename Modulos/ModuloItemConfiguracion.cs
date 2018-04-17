3a1254a39ffb5c3c966cb8b2dbc73bf24da19f53
AdrianInsomnio <appxims@gmail.com>
2018-04-17 12:25:41 -0300

    Add files via upload

---
+++ Modulos/ModuloItemConfiguracion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+
+namespace CapaModelo.Modulos
+{
+   public class ModuloItemConfiguracion
+    {
+        private ObservableCollection<Modelo.ItemConfiguracion> m_colleccion;
+
+        #region Propiedades
+
+        #endregion
+        #region Constructores
+
+        #endregion
+        #region AccesoDatos
+
+        #endregion
+    }
+}
