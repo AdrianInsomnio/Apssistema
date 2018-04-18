b646ccb3c21d2f9a1d32e7cfb35057530494ff5e
AdrianInsomnio <appxims@gmail.com>
2018-04-18 15:17:45 -0300

    Add files via upload

---
+++ Repositorio/IDataBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo.Repositorio
+{
+    interface IDataBase
+    {
+        bool Estado();
+        bool AbrirConexion( string abrCon);
+
+        bool CerrarConexion(string cerCon);
+    }
+}
