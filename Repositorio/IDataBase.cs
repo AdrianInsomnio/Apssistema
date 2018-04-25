5543404c09e2a834420851fdee2305029dc03b1e
AdrianInsomnio <appxims@gmail.com>
2018-04-25 15:31:29 -0300

    Add files via upload

--- Repositorio/IDataBase.cs
+++ Repositorio/IDataBase.cs
@@ -8,9 +8,26 @@ namespace CapaModelo.Repositorio
 {
     interface IDataBase
     {
+        /// <summary>
+        /// retorna si la conexion estra Abierta = true o cerrada=False
+        /// </summary>
+        /// <returns></returns>
         bool Estado();
+
+        /// <summary>
+        /// pre-Condicion la conexion esta Cerrada 
+        /// true si abre sastifactorio false si hubo algun error 
+        /// </summary>
+        /// <param name="abrCon"></param>
+        /// <returns></returns>
         bool AbrirConexion( string abrCon);
 
+        /// <summary>
+        /// true cierra la conexion si esta cerrada ya true 
+        /// false si la conexion queda abierta
+        /// </summary>
+        /// <param name="cerCon"></param>
+        /// <returns></returns>
         bool CerrarConexion(string cerCon);
     }
 }
---
+++ Repositorio/RepoItemConfiguracion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaModelo.Modelo;
+
+namespace CapaModelo.Repositorio
+{
+   public class RepoItemConfiguracion : IDataBase ,IRepo<Modelo.ItemConfiguracion>
+    {
+        private string m_Conexion = null;
+        public RepoItemConfiguracion(string conn)
+        {
+            m_Conexion = conn;
+        }
+
+
+
+
+        public bool AbrirConexion(string abrCon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Actualizar(ItemConfiguracion a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Borrar(int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<ItemConfiguracion> Buscar(string search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CerrarConexion(string cerCon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Estado()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insertar(ItemConfiguracion i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
