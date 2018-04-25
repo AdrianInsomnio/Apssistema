b29bae50eb32011f10ee599be228f68c207fea7b
AdrianInsomnio <appxims@gmail.com>
2018-04-25 10:23:01 -0300

    Add files via upload

---
+++ Repositorio/IRepoList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo.Repositorio
+{
+    interface IRepoList<T> where T : class
+    {
+        int Insertar(T i);
+        int Actualizar(T a);
+        int Borrar(int b);
+        List<T> Buscar(string search);
+    }
+}
